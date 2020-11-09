e512bad881a0fba3f69f5b5cef122c808952a6d4
alexeiablis06081980 <74046418+alexeiablis06081980@users.noreply.github.com>
2020-11-09 09:10:33 +0300

    Update main.java

--- tasc_1.1/main.java
+++ tasc_1.1/main.java
@@ -3,3 +3,8 @@
         int b = 7;
          String s;?
         System.out.println(a+b);   System.out.println("s")
+ 4
+                 public static void main(String[] args) {
+        int number  = 25;
+        int number1 = 2;
+        System.out.print(number1 * number);
